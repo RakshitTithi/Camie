450893713f335382c7643da6be0b91ee9c2b121f
Seungmi Park <ellasmpark@gmail.com>
2022-11-22 20:03:26 +0100

    setting up a DL model

--- model.py
+++ model.py
@@ -59,6 +59,105 @@ base_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accur
 
 base_model.summary()
 
+root='/content/drive/MyDrive/Colab Notebooks/ICYO/Shaghaitech dataset/ShanghaiTech'
+
+part_A_train=os.path.join(root,'part_A/train_data','images')
+part_A_test = os.path.join(root,'part_A/test_data','images')
+part_B_train = os.path.join(root,'part_B/train_data','images')
+part_B_test = os.path.join(root,'part_B/test_data','images')
+path_sets = [part_A_train,part_A_test]
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+root
+
+path_sets
+
+img_paths=[]
+for path in path_sets:
+    for img_path in sorted(glob.glob(os.path.join(path,'*.jpg'))):
+        img_paths.append(img_path)
+
+test_path = img_paths[1]
+test_path
+
+# img_paths
+
+def gaussian_filter_density(gt):
+    print (gt.shape)
+    density = np.zeros(gt.shape, dtype=np.float32)
+    gt_count = np.count_nonzero(gt)
+    if gt_count == 0:
+        return density
+
+    pts = np.array(list(zip(np.nonzero(gt)[1], np.nonzero(gt)[0])))
+    leafsize = 2048
+    # build kdtree
+    tree = scipy.spatial.KDTree(pts.copy(), leafsize=leafsize)
+    # query kdtree
+    distances, locations = tree.query(pts, k=4)
+
+    print ('generate density...')
+    for i, pt in enumerate(pts):
+        pt2d = np.zeros(gt.shape, dtype=np.float32)
+        pt2d[pt[1],pt[0]] = 1.
+        if gt_count > 1:
+            sigma = (distances[i][1]+distances[i][2]+distances[i][3])*0.1
+        else:
+            sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
+        density += scipy.ndimage.filters.gaussian_filter(pt2d, sigma, mode='constant')
+    print ('done.')
+    return density
+
+# for img_path in img_paths:
+# #    print(img_path)
+#     #loading mat files and modifying the names as per the corresponding images
+mat=io.loadmat(test_path.replace('.jpg','.mat').replace('images','ground-truth').replace('IMG_','GT_IMG_'))
+img=plt.imread(test_path) #reading the image as a tensor
+k = np.zeros((img.shape[0],img.shape[1])) #creating an array of 0s
+gt=mat['image_info'][0,0][0,0][0] #accessing ground truth elements
+for i in range(0,len(gt)):
+    if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
+        k[int(gt[i][1]),int(gt[i][0])]=1
+k = gaussian_filter_density(k)
+with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground-truth'), 'w') as hf:
+        hf['density'] = k
+
+
+
+# hf['density']
+
+h5py.File(hf, 'r')
+
+density = np.zeros(gt.shape, dtype=np.float32)
+
+gt_count = np.count_nonzero(gt)
+gt_count
+
+# if gt_count == 0:
+#   return density
+
+pts = np.array(list(zip(np.nonzero(gt)[1], np.nonzero(gt)[0])))
+pts
+
+pts.shape
+
+h5_root = '/content/drive/MyDrive/Colab Notebooks/ICYO/Shaghaitech dataset/shanghaitech_h5_empty/ShanghaiTech'
+
+h5_part_A_train = os.path.join(h5_root,'part_A/train_data','images')
+
+h5_test = '/content/drive/MyDrive/Colab Notebooks/ICYO/Shaghaitech dataset/shanghaitech_h5_empty/ShanghaiTech/part_A/train_data/images/IMG_1.jpg'
+
+# # file_path = img_paths[0].replace('.jpg','.h5').replace('images','ground') 
+# # print(file_path)
+# type(file_path)
+
+gt_file = h5py.File(file_path,'r')
+groundtruth = np.asarray(gt_file['density'])
+plt.imshow(groundtruth,cmap=CM.jet)
+print("Sum = " ,np.sum(groundtruth))
+
 #CNN Model with transferred learning from VGG16
 # def build_model():
 #instantiate the model
@@ -117,91 +216,3 @@ vgg_16_model.fit(img, gt)
 
 
 # vgg_16_model.summary()
-
-root='/content/drive/MyDrive/Colab Notebooks/ICYO/Shaghaitech dataset/ShanghaiTech'
-
-part_A_train=os.path.join(root,'part_A/train_data','images')
-part_A_test = os.path.join(root,'part_A/test_data','images')
-part_B_train = os.path.join(root,'part_B/train_data','images')
-part_B_test = os.path.join(root,'part_B/test_data','images')
-path_sets = [part_A_train,part_A_test]
-
-from google.colab import drive
-drive.mount('/content/drive')
-
-root
-
-path_sets
-
-img_paths=[]
-for path in path_sets:
-    for img_path in sorted(glob.glob(os.path.join(path,'*.jpg'))):
-        img_paths.append(img_path)
-
-test_path = img_paths[1]
-test_path
-
-# img_paths
-
-# for img_path in img_paths:
-# #    print(img_path)
-#     #loading mat files and modifying the names as per the corresponding images
-mat=io.loadmat(test_path.replace('.jpg','.mat').replace('images','ground-truth').replace('IMG_','GT_IMG_'))
-img=plt.imread(test_path) #reading the image as a tensor
-k = np.zeros((img.shape[0],img.shape[1])) #creating an array of 0s
-gt=mat['image_info'][0,0][0,0][0] #accessing ground truth elements
-for i in range(0,len(gt)):
-    if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
-        k[int(gt[i][1]),int(gt[i][0])]=1
-k = gaussian_filter_density(k)
-with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground-truth'), 'w') as hf:
-        hf['density'] = k
-
-gt.shape
-
-density = np.zeros(gt.shape, dtype=np.float32)
-
-gt_count = np.count_nonzero(gt)
-gt_count
-
-# if gt_count == 0:
-#   return density
-
-pts = np.array(list(zip(np.nonzero(gt)[1], np.nonzero(gt)[0])))
-pts
-
-pts.shape
-
-def gaussian_filter_density(gt):
-    print (gt.shape)
-    density = np.zeros(gt.shape, dtype=np.float32)
-    gt_count = np.count_nonzero(gt)
-    if gt_count == 0:
-        return density
-
-    pts = np.array(list(zip(np.nonzero(gt)[1], np.nonzero(gt)[0])))
-    leafsize = 2048
-    # build kdtree
-    tree = scipy.spatial.KDTree(pts.copy(), leafsize=leafsize)
-    # query kdtree
-    distances, locations = tree.query(pts, k=4)
-
-    print ('generate density...')
-    for i, pt in enumerate(pts):
-        pt2d = np.zeros(gt.shape, dtype=np.float32)
-        pt2d[pt[1],pt[0]] = 1.
-        if gt_count > 1:
-            sigma = (distances[i][1]+distances[i][2]+distances[i][3])*0.1
-        else:
-            sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
-        density += scipy.ndimage.filters.gaussian_filter(pt2d, sigma, mode='constant')
-    print ('done.')
-    return density
-
-file_path = img_paths[0].replace('.jpg','.h5').replace('images','ground') 
-print(file_path)
-
-gt_file = h5py.File(file_path,'r')
-groundtruth = np.asarray(gt_file['density'])
-plt.imshow(groundtruth,cmap=CM.jet)
-print("Sum = " ,np.sum(groundtruth))
